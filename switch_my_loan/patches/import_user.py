232c20aad12c5be13abbe172bde351dc661a1d2b
Harish Tanwar <harish.tanwar@atriina.com>
2022-04-13 15:54:58 +0530

    Updating Patches.txt

--- switch_my_loan/patches/import_user.py
+++
@@ -1,6 +0,0 @@
-import frappe
-from frappe.core.doctype.data_import.data_import import import_file
-
-def execute():
-    path = frappe.get_app_path('switch_my_loan','patches','master_imports','user.csv')
-    import_file('User', path, 'Insert',console=True)
