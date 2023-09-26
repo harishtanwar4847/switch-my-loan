9651e6e6e715a86bdaf03cdf02adf96da1cdd6b4
Abdul Wadudh <abdulwadudh5@gmail.com>
2023-09-26 16:44:26 +0530

    feat: :sparkles: Updated the Nucleus Base URI

--- switch_my_loan/boot.py
+++ switch_my_loan/boot.py
@@ -2,4 +2,4 @@ import os
 
 def boot_session(bootinfo):
     bootinfo.sml_encryption_key = os.getenv("SML_ENCRYPTION_KEY") or "3JHSSN8XWY0SL3I3"
-    bootinfo.sml_nucleus_base_url = os.getenv("SML_NUCLEUS_BASE_URL") or "http://nucleus.switchmyloan.in:3000"
+    bootinfo.sml_nucleus_base_url = os.getenv("SML_NUCLEUS_BASE_URL") or "https://nucleus.switchmyloan.in"
