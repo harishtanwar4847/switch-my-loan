36f0da7c03454ba1e668906c4a500ed870e5b774
Abdul Wadudh <abdulwadudh5@gmail.com>
2023-09-26 12:16:27 +0530

    feat: :sparkles: Added the Notification and Disabled the SMS

--- switch_my_loan/boot.py
+++ switch_my_loan/boot.py
@@ -1,5 +1,5 @@
 import os
 
 def boot_session(bootinfo):
-    bootinfo.sml_encryption_key = os.getenv("SML_ENCRYPTION_KEY") or "AAAAAAAAZZZZZZZZ"
-    bootinfo.sml_nucleus_base_url = os.getenv("SML_NUCLEUS_BASE_URL") or "http://localhost:3000"
+    bootinfo.sml_encryption_key = os.getenv("SML_ENCRYPTION_KEY") or "3JHSSN8XWY0SL3I3"
+    bootinfo.sml_nucleus_base_url = os.getenv("SML_NUCLEUS_BASE_URL") or "http://nucleus.switchmyloan.in:3000"
